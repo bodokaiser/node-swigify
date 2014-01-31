839c6add787bc7d07b7fb061f082533e09082eea
Bodo Kaiser <i@bodokaiser.io>
2014-01-31 16:06:36 +0100

    precompiling includes in transform

--- opt/test/index.js
+++ opt/test/index.js
@@ -8,39 +8,14 @@ var include = require('../views/include/parent.html');
 
 describe('swigify', function() {
 
-  before(function() {
-    swig.setDefaults({
-      loader: {
-        resolve: function(target, source) {
-          var dirname = path.dirname(source);
-
-          return path.join(dirname, target);
-        },
-        load: function(target) {
-          if (target.indexOf('.')) {
-            target = './' + target;
-          }
-          return require(target);
-        }
-      }
-    });
-  });
-
   it('should render simple template', function() {
-    var result = swig.render(simple, {
-      filename: '../views/simple/hello.html'
-    });
+    var result = swig.render(simple);
 
     chai.expect(result).to.contain('<h1>Hello World!</h1>');
   });
 
   it('should render included template', function() {
-    var name = '../views/include/child.html';
-
-    console.log(require(name));
-
     var result = swig.render(include, {
-      filename: '../views/include/parent.html',
       locals: { name: 'Bodo' }
     });
 
@@ -48,9 +23,7 @@ describe('swigify', function() {
   });
 
   xit('should render child with extended parent', function() {
-    var result = swig.render(extend, {
-      filename: '../views/extends/child.html'
-    });
+    var result = swig.render(extend);
 
     chai.expect(result).to.contain('<body>');
     chai.expect(result).to.contain('<img src="foobar.jpeg" />');
